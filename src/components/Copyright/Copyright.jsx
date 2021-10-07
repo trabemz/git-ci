890da1a8cde322d9c577f36a5d6fc0934463915c
Anastasia Zamkovskaya <trabem.z@gmail.com>
2021-10-07 15:12:58 +0300

    Add Copyright component

---
+++ src/components/Copyright/Copyright.jsx
@@ -0,0 +1,17 @@
+import {React} from 'react';
+import {PropTypes} from 'prop-types';
+
+import './Copyright.css';
+
+export default function Copyright(props) {
+  const {children, className} = props;
+  const currYear = new Date().getFullYear();
+  return (
+    <span className={`copyright ${className}`}>© {currYear} {children}</span>
+  );
+}
+
+Copyright.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string
+}
