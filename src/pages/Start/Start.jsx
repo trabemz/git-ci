9ee1d7d038a4c959de82913376290d636bb6c164
Anastasia Zamkovskaya <trabem.z@gmail.com>
2021-10-07 19:11:02 +0300

    Start section

--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -1,5 +1,7 @@
 import {Header} from '../../components/Header/Header';
 import {React} from 'react';
+import './Start.css'
+import { ReactComponent as ConfigureLogo } from '../../assets/icons/configure-logo.svg';
 
 export function Start(){
   return (
@@ -7,8 +9,13 @@ export function Start(){
       <Header>
         <h1 className='header__logo'>School CI server</h1>
       </Header>
-      <main>
-
+      <main className="start">
+        <section className="start__section">
+            <ConfigureLogo />
+            <p>Configure repository connection <br/> 
+              and synchronization settings</p>
+            <button>Open settings</button> 
+        </section>
       </main>
     </>
   );
