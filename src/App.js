81583219b75a49d627648073b84328180654d2f5
Anastasia Zamkovskaya <trabem.z@gmail.com>
2021-10-08 01:30:59 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Footer } from "./components/Footer/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SettingsContextProvider } from "./store/settingsContext";
 import { Home } from "./pages/Home/Home";
+import { Settings } from "./pages/Settings/Settings";
+import { History } from "./pages/History/History";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
+          <Route path="/settings" component={Settings}></Route>
+          <Route path="/history" component={History}></Route>
         </Switch>
       </SettingsContextProvider>
       <Footer />
