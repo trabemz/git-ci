9d15ce3196fc8420b0941f8c36e498b40a3609b6
Anastasia Zamkovskaya <trabem.z@gmail.com>
2021-10-07 11:53:08 +0300

    Install dev infrastructure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
