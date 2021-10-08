6c1e0d2686a9ed917d0445603564d321cadf5406
Anastasia Zamkovskaya <trabem.z@gmail.com>
2021-10-09 00:45:07 +0300

    Build card (mobile)

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import { ToolButton } from '../../components/ToolButton/ToolButton';
 import { ReactComponent as SettingsIcon } from '../../assets/icons/settings.svg';
 import { ReactComponent as RunIcon } from '../../assets/icons/play.svg';
+import { ReactComponent as DoneIcon } from '../../assets/icons/done.svg'
 
 import './History.css';
 
@@ -20,7 +21,7 @@ export function History(){
   return (
     <>
       <Header>
-      <h1 className='header__title--primary'>philip1967/my-awesome-repo-with-long-long-long-repo-name</h1>
+      <h1 className='header__title--primary'>philip1967/my-aw esome-repo-with-long-long-long-repo-name</h1>
       <div className='header__tool-buttons'>
         <ToolButton text='Run build' handleClick={handleRunClick}>
           <RunIcon className='tool-button__icon'/>
@@ -30,6 +31,27 @@ export function History(){
         </ToolButton>
       </div>
       </Header>
+      <main className='container'>
+        <div className='build'>
+          <div className='build__icon-container'>
+            <DoneIcon className='build__done'/>
+          </div>
+          <div className='build__information-container'>
+              <span className='build__id'>#1368</span>
+              <span className='build__title'>add documentation for postgres scaler</span>
+              <span className='build__head-container'><span className='build__branch'>master</span> <span className='build__hash'>9c9f0b9</span></span>
+              <span className='build__author'>Philip Kirkorov</span>
+          </div>
+          <div className='build__time-container'>
+            <span className='build__date'>
+              21 янв, 03:06
+            </span>
+            <span className='build__buildTime'>
+              1 ч 20 мин
+            </span>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
